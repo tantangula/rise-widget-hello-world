c18aca031f7882d55a7c8337b1d0906ab28277b1
Donna Murphy <donna.peplinskie@risevision.com>
2014-06-12 09:43:13 -0400

    Save background colour and code refactoring

--- js/settings.js
+++ js/settings.js
@@ -6,24 +6,27 @@ RiseVision.Text.Settings = (function($, gadgets) {
   "use strict";
 
   var _editor = null;
-  var textColor = "";
+  var $_editable = $("#editable");
+  var $_fontPicker, $_fontSizePicker;
+  var $_textColor, $_highlightColor, $_backgroundColor;
   var FONT_SIZE_PICKER_STYLESHEET =
     "http://s3.amazonaws.com/rise-common-test/styles/bootstrap-font-size-picker/bootstrap-font-size-picker.css";
   var HELP_URL =
     "http://www.risevision.com/help/users/what-are-gadgets/content/playlist-item-text-editor/";
 
   function _getSettings() {
-    var settings = null,  params = "", additionalParams = {};
+    var settings = null, additionalParams = {};
     var backgroundColor = _editor.composer.doc.body
       .getAttribute("data-background-color");
 
     $(".errors").empty();
 
-    params = "&up_background-color=" + backgroundColor;
-    additionalParams.data = $("#editable").val();
+    // Put background color in additionalParams due to RVA issue 1075.
+    additionalParams["background-color"] = backgroundColor;
+    additionalParams.data = $_editable.val();
 
     settings = {
-      "params": params,
+      "params": null,
       "additionalParams": JSON.stringify(additionalParams)
     };
 
@@ -34,8 +37,8 @@ RiseVision.Text.Settings = (function($, gadgets) {
 
   function _configureColorPicker(options) {
     options.elem.spectrum({
-      color: "#000",
       type: options.type,
+      color: options.color,
       showInput: true,
       chooseText: "Apply",
       cancelText: "Cancel",
@@ -50,11 +53,120 @@ RiseVision.Text.Settings = (function($, gadgets) {
     });
   }
 
-  function init() {
-    var $editable = $("#editable");
+  function _bind() {
+    $("#save").on("click", function() {
+      _getSettings();
+    });
 
+    $("#cancel, #close").on("click", function() {
+      gadgets.rpc.call("", "rscmd_closeSettings", null);
+    });
+
+    $("#help").on("click", function() {
+      window.open(HELP_URL, "_blank");
+    });
+
+    // Font picker
+    $($_fontPicker.data("plugin_fontPicker"))
+      .on("standardFontSelected", function(e, font, fontFamily) {
+        _editor.composer.commands.exec("standardFont", font, fontFamily, [{
+          name: "data-standard-font",
+          value: font
+        },
+        {
+          name: "data-standard-font-family",
+          value: fontFamily
+        }
+        ]);
+
+        $_editable.focus();
+      })
+      .on("googleFontSelected", function(e, font) {
+        _editor.composer.commands.exec("googleFont", font, [{
+          name: "data-google-font",
+          value: font
+        }]);
+
+        $_editable.focus();
+      })
+      .on("customFontSelected", function(e, font, fontURL) {
+        _editor.composer.commands.exec("customFont", font, [{
+          name: "data-custom-font",
+          value: font
+        },
+        {
+          name: "data-custom-font-url",
+          value: fontURL
+        }
+        ]);
+
+        $_editable.focus();
+      });
+
+    // Font size picker
+    $($_fontSizePicker.data("plugin_fontSizePicker"))
+      .on("change.bfhselectbox", function() {
+        _editor.composer.commands.exec("fontSize",
+          $(this).find(".bfh-fontsizes").val());
+      });
+
+    // When the user clicks in the editor, set toolbar to match text styles.
+    $(".wysihtml5-sandbox").contents().find("body").on("click", function() {
+      var font = "", fontSize = "";
+      var color = "", highlightColor = "";
+      var node = null, parentNode = null;
+
+      // Hide color pickers.
+      $_textColor.spectrum("hide");
+      $_highlightColor.spectrum("hide");
+      $_backgroundColor.spectrum("hide");
+
+      node = _editor.composer.selection.getSelectedNode();
+
+      if (node) {
+        // This is a text node.
+        if (node.nodeType === 3) {
+          parentNode = node.parentNode;
+
+          // The parent node is an element.
+          if (parentNode && parentNode.nodeType === 1) {
+            // The parent node is not the editor element itself.
+            if (parentNode.tagName.toLowerCase() !== "body") {
+              font = window.getComputedStyle(parentNode, null)
+                .getPropertyValue("font-family");
+              fontSize = window.getComputedStyle(parentNode, null)
+                .getPropertyValue("font-size");
+              color = window.getComputedStyle(parentNode, null)
+                .getPropertyValue("color");
+              highlightColor = window.getComputedStyle(parentNode, null)
+                .getPropertyValue("background-color");
+
+              if (font) {
+                $_fontPicker.data("plugin_fontPicker").setFont(font);
+              }
+
+              if (fontSize) {
+                $_fontSizePicker.data("plugin_fontSizePicker")
+                  .setFontSize(fontSize);
+              }
+
+              if (color) {
+                $_textColor.spectrum("set", color);
+              }
+
+              if (highlightColor) {
+                $_highlightColor.spectrum("set", highlightColor);
+              }
+            }
+          }
+        }
+      }
+    });
+  }
+
+  function init() {
     // Configure editor toolbar.
-    $editable.wysihtml5({
+    $_editable.wysihtml5({
       "toolbar": {
         "text-color":
            "<li>" +
@@ -88,169 +200,72 @@ RiseVision.Text.Settings = (function($, gadgets) {
       "stylesheets": [FONT_SIZE_PICKER_STYLESHEET],
     });
 
-    // When the user clicks in the editor, find the font and font size
-    // for that element and set those values in the appropriate dropdowns.
-    $(".wysihtml5-sandbox").contents().find("body").on("click", function() {
-      var font = "", fontSize = "";
-      var color = "", highlightColor = "";
-      var node = null, parentNode = null;
+    _editor = $_editable.data("wysihtml5").editor;
+    $_fontPicker = $(".font-picker");
+    $_fontSizePicker = $(".font-size-picker");
+    $_textColor = $("#text-color");
+    $_highlightColor = $("#highlight-color");
+    $_backgroundColor = $("#background-color");
 
-      // Hide color pickers.
-      $("#text-color, #highlight-color, #background-color").spectrum("hide");
-
-      node = _editor.composer.selection.getSelectedNode();
-
-      if (node) {
-        // This is a text node.
-        if (node.nodeType === 3) {
-          parentNode = node.parentNode;
-
-          // The parent node is an element.
-          if (parentNode && parentNode.nodeType === 1) {
-            // The parent node is not the editor element itself.
-            if (parentNode.tagName.toLowerCase() !== "body") {
-              font = window.getComputedStyle(parentNode, null)
-                .getPropertyValue("font-family");
-              fontSize = window.getComputedStyle(parentNode, null)
-                .getPropertyValue("font-size");
-              color = window.getComputedStyle(parentNode, null)
-                .getPropertyValue("color");
-              highlightColor = window.getComputedStyle(parentNode, null)
-                .getPropertyValue("background-color");
-
-              if (font) {
-                $(".font-picker").data("plugin_fontPicker").setFont(font);
-              }
-
-              if (fontSize) {
-                $(".font-size-picker").data("plugin_fontSizePicker")
-                  .setFontSize(fontSize);
-              }
-
-              if (color) {
-                $("#text-color").spectrum("set", color);
-              }
-
-              if (highlightColor) {
-                $("#highlight-color").spectrum("set", highlightColor);
-              }
-            }
-          }
-        }
-      }
+    // Configure the font picker.
+    $_fontPicker.fontPicker({
+      "contentDocument": _editor.composer.iframe.contentDocument
     });
 
-    _editor = $editable.data("wysihtml5").editor;
+    // Configure the font size picker.
+    $_fontSizePicker.fontSizePicker({});
 
-    // Initialize the font picker.
-    $(".font-picker").fontPicker({
-      "contentDocument": _editor.composer.iframe.contentDocument
-    })
-      .on("standardFontSelected", function(e, font, fontFamily) {
-        _editor.composer.commands.exec("standardFont", font, fontFamily, [{
-          name: "data-standard-font",
-          value: font
-        },
-        {
-          name: "data-standard-font-family",
-          value: fontFamily
-        }
-        ]);
-
-        $editable.focus();
-      })
-      .on("googleFontSelected", function(e, font) {
-        _editor.composer.commands.exec("googleFont", font, [{
-          name: "data-google-font",
-          value: font
-        }]);
-
-        $editable.focus();
-      })
-      .on("customFontSelected", function(e, font, fontURL) {
-        _editor.composer.commands.exec("customFont", font, [{
-          name: "data-custom-font",
-          value: font
-        },
-        {
-          name: "data-custom-font-url",
-          value: fontURL
-        }
-        ]);
-
-        $editable.focus();
-      });
-
-    // Initialize the font size picker.
-    $(".font-size-picker").fontSizePicker({})
-      .on("change.bfhselectbox", function() {
-        _editor.composer.commands.exec("fontSize",
-          $(this).find(".bfh-fontsizes").val());
-      });
-
-    // Initialize the color pickers.
+    // Configure the color pickers.
     _configureColorPicker({
-      elem: $("#text-color"),
+      elem: $_textColor,
       type: "text",
+      color: "#000",
       command: "textColor",
       attribute: "data-text-color"
     });
 
     _configureColorPicker({
-      elem: $("#highlight-color"),
+      elem: $_highlightColor,
       type: "background",
+      color: "transparent",
       command: "highlightColor",
       attribute: "data-highlight-color"
     });
 
     _configureColorPicker({
-      elem: $("#background-color"),
+      elem: $_backgroundColor,
       type: "background",
+      color: "#fff",
       command: "backgroundColor",
       attribute: "data-background-color"
     });
 
-    i18n.init(function(t) {
-      $(".widget-wrapper").i18n().show();
-
-      // Set buttons to be sticky only after wrapper is visible.
-      $(".buttons").sticky({
-        container : $("#wrapper"),
-        topSpacing : 41
-      });
-    });
-
-    $("#save").on("click", function() {
-      _getSettings();
-    });
-
-    $("#cancel, #close").on("click", function() {
-      gadgets.rpc.call("", "rscmd_closeSettings", null);
-    });
-
-    $("#help").on("click", function() {
-      window.open(HELP_URL, "_blank");
-    });
+    _bind();
 
     $(".alert").hide();
 
-    //Request additional parameters from the Viewer.
+    // Request additional parameters from the Viewer.
     gadgets.rpc.call("", "rscmd_getAdditionalParams", function(result) {
       var prefs = new gadgets.Prefs();
       var util = RiseVision.Common.Utilities;
       var standardFont, googleFont, customFont;
-      var textColor, highlightColor;
-      var backgroundColor = prefs.getString("background-color");
-
-      if (backgroundColor) {
-        _editor.composer.commands.exec("backgroundColor", backgroundColor);
-      }
+      var textColor, highlightColor, backgroundColor;
 
       // Settings have been saved before.
       if (result) {
         result = JSON.parse(result);
 
         _editor.setValue(result.data);
+
+        backgroundColor = result["background-color"];
+
+        if (backgroundColor) {
+          $_backgroundColor.spectrum("set", backgroundColor);
+          _editor.composer.commands.exec("backgroundColor", backgroundColor, [{
+            name: "data-background-color",
+            value: backgroundColor
+          }]);
+        }
 
         $.each($(result.data).find("span").andSelf(), function(index, value) {
           standardFont = $(this).attr("data-standard-font");
@@ -267,7 +282,7 @@ RiseVision.Text.Settings = (function($, gadgets) {
 
           // Load and add CSS for Google fonts.
           if (googleFont) {
-            $(".font-picker").data("plugin_fontPicker")
+            $_fontPicker.data("plugin_fontPicker")
               .addGoogleFont(googleFont, false);
 
             // This won't add a new span tag because a range will not have been
@@ -293,6 +308,16 @@ RiseVision.Text.Settings = (function($, gadgets) {
           }
         });
       }
+
+      i18n.init(function(t) {
+        $(".widget-wrapper").i18n().show();
+
+        // Set buttons to be sticky only after wrapper is visible.
+        $(".buttons").sticky({
+          container : $("#wrapper"),
+          topSpacing : 41
+        });
+      });
     });
   }
 
