111705063bf62c81680de91aa41185aba651fe07
Donna Murphy <donna.peplinskie@risevision.com>
2014-09-29 12:31:27 -0400

    Simplify e2e tests

--- test/e2e/text-settings-scenarios.js
+++ test/e2e/text-settings-scenarios.js
@@ -7,9 +7,6 @@
   var expect;
   var chai = require("chai");
   var chaiAsPromised = require("chai-as-promised");
-  var FONT_PICKER_COUNT = 48;
-  var FONT_SIZE_PICKER_COUNT = 14;
-  var LINE_HEIGHT_COUNT = 4;
 
   chai.use(chaiAsPromised);
   expect = chai.expect;
@@ -22,97 +19,22 @@
     });
 
     it("Should load scroll component", function () {
-      expect(element(by.id("scroll-by")).isPresent()).
-        to.eventually.be.true;
+      expect(element(by.id("scroll-by")).isPresent())
+        .to.eventually.be.true;
 
       expect(element(by.id("scroll-by")).getAttribute("value"))
         .to.eventually.equal("none");
     });
 
-    it("Should load clear button", function () {
-      expect(element(by.css(".clear")).isPresent()).to.eventually.be.true;
-    });
-
-    it("Should load font styles", function () {
-      expect(element(by.css(".emphasis .bold")).isPresent())
+    it("Should load editor", function () {
+      expect(element(by.css(".wysihtml5-toolbar")).isPresent())
         .to.eventually.be.true;
 
-      expect(element(by.css(".emphasis .italic")).isPresent())
+      expect(element(by.css(".wysihtml5-sandbox")).isPresent())
         .to.eventually.be.true;
 
-      expect(element(by.css(".emphasis .underline")).isPresent())
-        .to.eventually.be.true;
-
-      expect(element(by.css(".emphasis .wysihtml5-command-active")).isPresent())
+      expect(element(by.id("editable")).isDisplayed())
         .to.eventually.be.false;
-    });
-
-    it("Should load alignment component", function () {
-      expect(element(by.css(".btn-alignment .fa-align-left"))
-        .isPresent()).to.eventually.be.true;
-
-      expect(element(by.css(
-        ".btn-alignment[data-wysihtml5-command-value='left']")).isPresent())
-        .to.eventually.be.true;
-    });
-
-    it("Should load font picker", function () {
-      expect(element(by.css(".font-picker .bfh-selectbox"))
-        .isPresent()).to.eventually.be.true;
-
-      expect(element(by.css(".font-picker .google-fonts"))
-        .isPresent()).to.eventually.be.true;
-
-      expect(element(by.css(".font-picker .custom-font"))
-        .isPresent()).to.eventually.be.true;
-
-      expect(element(by.css(".font-picker .google-fonts"))
-        .isDisplayed()).to.eventually.be.false;
-
-      expect(element(by.css(".font-picker .custom-font"))
-        .isDisplayed()).to.eventually.be.false;
-
-      expect(element.all(by.css(".font-picker .bfh-selectbox ul li"))
-        .count()).to.eventually.equal(FONT_PICKER_COUNT);
-    });
-
-    it("Should load font size picker", function () {
-      expect(element(by.css(".font-size-picker .bfh-fontsizes"))
-        .isPresent()).to.eventually.be.true;
-
-      expect(element.all(by.css(".font-size-picker .bfh-fontsizes ul li"))
-        .count()).to.eventually.equal(FONT_SIZE_PICKER_COUNT);
-    });
-
-    it("Should load line height", function () {
-      expect(element(by.css(".line-height")).isPresent()).to.eventually.be.true;
-
-      expect(element.all(by.css(".line-height ul li")).count())
-        .to.eventually.equal(LINE_HEIGHT_COUNT);
-    });
-
-    it("Should load text colour", function () {
-      expect(element(by.css(".text-color")).getCssValue("display")).
-        to.eventually.equal("none");
-
-      expect(element(by.css(".text-color-picker")).isPresent()).
-        to.eventually.be.true;
-    });
-
-    it("Should load highlight colour", function () {
-      expect(element(by.css(".highlight-color")).getCssValue("display")).
-        to.eventually.equal("none");
-
-      expect(element(by.css(".highlight-color-picker")).isPresent()).
-        to.eventually.be.true;
-    });
-
-    it("Should load background colour", function () {
-      expect(element(by.css(".background-color")).getCssValue("display")).
-        to.eventually.equal("none");
-
-      expect(element(by.css(".background-color-picker")).isPresent()).
-        to.eventually.be.true;
     });
 
     it("Should correctly save settings", function() {
